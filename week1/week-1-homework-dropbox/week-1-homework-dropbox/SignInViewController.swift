d2fea50e4481a3f4c298ccbb186fcb3e11c29182
sayheyrickjames <rchatas@icloud.com>
2015-05-09 00:23:11 -0700

    updates

--- week1/week-1-homework-dropbox/week-1-homework-dropbox/SignInViewController.swift
+++ week1/week-1-homework-dropbox/week-1-homework-dropbox/SignInViewController.swift
@@ -14,6 +14,11 @@ class SignInViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func cancelButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
