e72d1b64d61246bd26d30f1d22ee795e8b274b76
sayheyrickjames <rchatas@icloud.com>
2015-05-16 22:20:47 -0700

    Cleaned up project.

--- week2/week2-homework-carousel/week2-homework-carouselTests/week2_homework_carouselTests.swift
+++ week2/week2-homework-carousel/week2-homework-carouselTests/week2_homework_carouselTests.swift
@@ -2,7 +2,7 @@
 //  week2_homework_carouselTests.swift
 //  week2-homework-carouselTests
 //
-//  Created by Rick James! Chatas on 5/16/15.
+//  Created by Rick James! Chatas on 5/11/15.
 //  Copyright (c) 2015 SayHey. All rights reserved.
 //
 
