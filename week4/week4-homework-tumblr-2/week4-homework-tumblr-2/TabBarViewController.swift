a698dd137e56de13fa50ee7e9e1aa77c4e46fe97
sayheyrickjames <rchatas@icloud.com>
2015-05-30 00:00:51 -0700

    Milestone 1 and 2

---
+++ week4/week4-homework-tumblr-2/week4-homework-tumblr-2/TabBarViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarViewController.swift
+//  week4-homework-tumblr-2
+//
+//  Created by Rick James! Chatas on 5/29/15.
+//  Copyright (c) 2015 SayHey. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    // outlets
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var composeButton: UIButton!
+    @IBOutlet weak var accountButton: UIButton!
+    @IBOutlet weak var trendingButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
