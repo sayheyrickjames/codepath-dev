a16e3989df0703394616db1378446226e44aa7ad
sayheyrickjames <rchatas@icloud.com>
2015-06-15 21:16:36 -0700

    Class work

---
+++ week7/week7-lab-chat/week7-lab-chat/LoginViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LoginViewController.swift
+//  week7-lab-chat
+//
+//  Created by Rick James! Chatas on 6/15/15.
+//  Copyright (c) 2015 SayHey. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressSignIn(sender: AnyObject) {
+        
+        PFUser.logInWithUsernameInBackground("Rick2", password:"awesome") {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+            } else {
+                // The login failed. Check error to see why.
+            }
+        }
+        
+    }
+    
+    @IBAction func didPressSignUp(sender: AnyObject) {
+        
+        var user = PFUser()
+        user.username = "Rick2"
+        user.password = "awesome"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo?["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
